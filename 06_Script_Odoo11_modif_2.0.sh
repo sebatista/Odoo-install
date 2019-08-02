0ee86ca32fcaeeadd7f17d1f776b831db134ae11
Sebas <sebatista@yahoo.com>
2019-08-01 23:47:52 -0300

    Se divide el script de instalación
    Se continua con la división del script de instalación.

---
+++ 06_Script_Odoo11_modif_2.0.sh
@@ -0,0 +1,48 @@
+#
+# Localizacion argentina ADHOC
+#
+
+# Crear directorios para modulos externos
+sudo mkdir /opt/odoo/addons
+cd /opt/odoo/addons
+
+sudo wget https://github.com/ingadhoc/odoo-argentina/archive/11.0.zip -O ingadhoc-odoo-argentina.zip
+sudo wget https://github.com/ingadhoc/account-financial-tools/archive/11.0.zip -O ingadhoc-account-financial-tools.zip
+sudo wget https://github.com/ingadhoc/account-payment/archive/11.0.zip -O ingadhoc-account-payment.zip
+sudo wget https://github.com/ingadhoc/aeroo_reports/archive/11.0.zip -O ingadhoc-aeroo_reports.zip
+sudo wget https://github.com/ingadhoc/miscellaneous/archive/11.0.zip -O ingadhoc-miscellaneous.zip
+sudo wget https://github.com/ingadhoc/argentina-reporting/archive/11.0.zip -O ingadhoc-argentina-reporting.zip
+sudo wget https://github.com/ingadhoc/reporting-engine/archive/11.0.zip -O ingadhoc-reporting-engine.zip
+sudo wget https://github.com/ingadhoc/argentina-sale/archive/11.0.zip -O ingadhoc-argentina-sale.zip
+sudo wget https://github.com/ingadhoc/stock/archive/11.0.zip -O ingadhoc-stock.zip
+sudo wget https://apps.odoo.com/loempia/download/partner_identification/11.0.1.0.0/4gXzqj52wzlBc64roBAw7v.zip -O partner_identification.zip
+
+sudo unzip ingadhoc-odoo-argentina.zip
+sudo unzip ingadhoc-account-financial-tools.zip
+sudo unzip ingadhoc-account-payment.zip
+sudo unzip ingadhoc-aeroo_reports.zip
+sudo unzip ingadhoc-miscellaneous.zip
+sudo unzip ingadhoc-argentina-reporting.zip
+sudo unzip ingadhoc-reporting-engine.zip
+sudo unzip ingadhoc-argentina-sale.zip
+sudo unzip ingadhoc-stock.zip
+sudo unzip partner_identification.zip
+
+sudo chown -R odoo:odoo -R /opt/odoo/addons
+sudo chmod -R 775 /opt/odoo/addons
+
+sudo -H pip3 install pycryptodome
+# original # 
+# sudo -H pip3 install -r account-payment-11.0/requirements.txt
+# sudo -H pip3 install -r aeroo_reports-11.0/requirements.txt
+# sudo -H pip3 install -r argentina-reporting-11.0/requirements.txt
+# sudo -H pip3 install -r argentina-sale-11.0/requirements.txt
+# sudo -H pip3 install -r miscellaneous-11.0/requirements.txt
+# sudo -H pip3 install -r odoo-argentina-11.0/requirements.txt
+sudo -H pip3 install -r account-payment-11.0/requirements.txt
+sudo -H pip3 install -r aeroo_reports-11.0/requirements.txt
+sudo -H pip3 install -r argentina-reporting-11.0/requirements.txt
+sudo -H pip3 install -r argentina-sale-11.0/requirements.txt
+sudo -H pip3 install -r miscellaneous-11.0/requirements.txt
+sudo -H pip3 install -r odoo-argentina-11.0/requirements.txt
+
---
+++ 07_Script_Odoo11_modif_2.0.sh
@@ -0,0 +1,83 @@
+#
+# Localizacion argentina OCA
+#
+
+# Crear directorios para modulos externos
+sudo -H mkdir /opt/odoo/addons/oca
+cd /opt/odoo/addons/oca
+
+sudo -H git clone https://github.com/OCA/server-tools -b 11.0 oca-server-tools
+sudo -H git clone https://github.com/OCA/sale-workflow -b 11.0 oca-sale-workflow
+sudo -H git clone https://github.com/OCA/sale-financial -b 11.0 oca-sale-financial
+sudo -H git clone https://github.com/OCA/purchase-workflow -b 11.0 oca-purchase-workflow
+sudo -H git clone https://github.com/OCA/account-financial-tools -b 11.0 oca-account-financial-tools
+sudo -H git clone https://github.com/OCA/account-financial-reporting -b 11.0 oca-account-financial-reporting
+sudo -H git clone https://github.com/OCA/account-invoicing -b 11.0 oca-account-invoicing
+sudo -H git clone https://github.com/OCA/account-payment -b 11.0 oca-account-payment
+sudo -H git clone https://github.com/OCA/account-analytic -b 11.0 oca-account-analytic
+sudo -H git clone https://github.com/OCA/reporting-engine -b 11.0 oca-reporting-engine
+sudo -H git clone https://github.com/OCA/stock-logistics-workflow -b 11.0 oca-stock-logistics-workflow
+sudo -H git clone https://github.com/OCA/stock-logistics-warehouse -b 11.0 oca-stock-logistics-warehouse
+sudo -H git clone https://github.com/OCA/knowledge -b 11.0 oca-knowledge
+sudo -H git clone https://github.com/OCA/manufacture -b 11.0 oca-manufacture
+sudo -H git clone https://github.com/OCA/margin-analysis -b 11.0 oca-margin-analysis
+sudo -H git clone https://github.com/OCA/crm -b 11.0 oca-crm
+sudo -H git clone https://github.com/OCA/hr -b 11.0 oca-hr
+sudo -H git clone https://github.com/OCA/hr-timesheet -b 11.0 oca-hr-timesheet
+sudo -H git clone https://github.com/OCA/commission -b 11.0 oca-commission
+sudo -H git clone https://github.com/OCA/report-print-send -b 11.0 oca-report-print-send
+sudo -H git clone https://github.com/OCA/social -b 11.0 oca-social
+sudo -H git clone https://github.com/OCA/contract -b 11.0 oca-contract
+sudo -H git clone https://github.com/OCA/web -b 11.0 oca-web
+sudo -H git clone https://github.com/OCA/website -b 11.0 oca-website
+sudo -H git clone https://github.com/OCA/product-variant -b 11.0 oca-product-variant
+sudo -H git clone https://github.com/OCA/product-attribute -b 11.0 oca-product-attribute
+sudo -H git clone https://github.com/OCA/partner-contact -b 11.0 oca-partner-contact
+sudo -H git clone https://github.com/OCA/e-commerce -b 11.0 oca-e-commerce
+
+sudo chown -R odoo:odoo -R /opt/odoo/addons
+sudo chmod -R 775 /opt/odoo/addons
+
+sudo -H pip3 install -r oca-server-tools/requirements.txt
+pip install -r oca-sale-workflow/requirements.txt
+pip install -r oca-sale-financial/requirements.txt
+pip install -r oca-purchase-workflow/requirements.txt
+pip install -r oca-account-financial-tools/requirements.txt
+pip install -r oca-account-financial-reporting/requirements.txt
+pip install -r oca-account-invoicing/requirements.txt
+pip install -r oca-account-payment/requirements.txt
+pip install -r oca-account-analytic/requirements.txt
+pip install -r oca-reporting-engine/requirements.txt
+pip install -r oca-stock-logistics-workflow/requirements.txt
+pip install -r oca-stock-logistics-warehouse/requirements.txt
+pip install -r oca-knowledge/requirements.txt
+pip install -r oca-manufacture/requirements.txt
+pip install -r oca-margin-analysis/requirements.txt
+pip install -r oca-crm/requirements.txt
+pip install -r oca-hr/requirements.txt
+pip install -r oca-hr-timesheet/requirements.txt
+pip install -r oca-commission/requirements.txt
+pip install -r oca-report-print-send/requirements.txt
+pip install -r oca-social/requirements.txt
+pip install -r oca-contract/requirements.txt
+pip install -r oca-web/requirements.txt
+pip install -r oca-website/requirements.txt
+pip install -r oca-product-variant/requirements.txt
+pip install -r oca-product-attribute/requirements.txt
+pip install -r oca-partner-contact/requirements.txt
+pip install -r oca-e-commerce/requirements.txt
+
+
+cd ..
+mkdir others
+cd others
+
+sudo -H git clone https://github.com/akretion/odoo-usability -b 11.0 others-akretion-odoo-usability
+sudo -H git clone https://github.com/it-projects-llc/website-addons -b 11.0 others-it-projects-llc-website-addons
+
+sudo -H pip install -r others-akretion-odoo-usability/requirements.txt
+sudo -H pip install -r others-it-projects-llc-website-addons/requirements.txt
+
+sudo chown -R odoo:odoo -R /opt/odoo/addons
+sudo chmod -R 775 /opt/odoo/addons
+
