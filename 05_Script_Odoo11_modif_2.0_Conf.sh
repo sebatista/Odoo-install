a8c6736df77c85abaaba014a4ff63d4c7497fa4a
Sebas <sebatista@yahoo.com>
2019-08-20 08:44:11 -0300

    Script de Ihstalación Odoo 11
    Se divide el archivo que crea la configuración de Odoo del servicio y reinicio.

--- 05_Script_Odoo11_modif_2.0_Conf.sh
+++ 05_Script_Odoo11_modif_2.0_Conf.sh
@@ -22,38 +22,3 @@ EOF
 sudo cp ~/odoo-server.conf /etc/odoo-server.conf
 sudo chown odoo: /etc/odoo-server.conf
 sudo chmod 640 /etc/odoo-server.conf
-
-
-# SERVICIO ODOO ver 2.0
-cat <<EOF > ~/odoo-server.service
-[Unit]
-Description=Odoo Open Source ERP and CRM
-
-[Service]
-Type=simple
-PermissionsStartOnly=true
-SyslogIdentifier=odoo-server
-User=odoo
-Group=odoo
-ExecStart=/opt/odoo/odoo-server/odoo-bin --config=/etc/odoo-server.conf
-WorkingDirectory=/opt/odoo/odoo-server/
-
-[Install]
-WantedBy=multi-user.target
-EOF
-
-sudo cp ~/odoo-server.service /lib/systemd/system/
-sudo chmod 755 /lib/systemd/system/odoo-server.service
-sudo chown root: /lib/systemd/system/odoo-server.service
-
-# Inicializamos
-sudo systemctl start odoo-server
-
-# Detenemos
-sudo systemctl stop odoo-server
-
-# Inicializar Automáticamente
-sudo systemctl enable odoo-server.service
-
-
-
