ed40e5b9b1283be34b49afae128fbcd79b1897c5
Sebas <sebatista@yahoo.com>
2020-01-28 10:38:51 -0300

    Se agregan Addons de Jobios y Regaby
    Se agregan addons y rutas en el archivo conf.

--- 73_Script_Odoo11_modif_2.0_addons_others.sh
+++ 73_Script_Odoo11_modif_2.0_addons_others.sh
@@ -23,21 +23,19 @@ fi
 cd /opt/odoo/addons/others
 
 
-sudo -H git clone https://github.com/akretion/odoo-usability -b 11.0 others-akretion-odoo-usability
-sudo -H git clone https://github.com/it-projects-llc/website-addons -b 11.0 others-it-projects-llc-website-addons
 # 2020-01-01 - Jobiols -> solo utilizarémos ARBA y SICORE.
-sudo -H git clone https://github.com/jobiols/odoo-addons -b 11.0 jobiols-addons
-# 2020-01-01 - Regaby -> solo utilizaremos 
-sudo -H git clone https://github.com/regaby/odoo-addons -b 11.0 regaby-addons
+# 2020-01-01 - Regaby -> solo utilizaremos ???
+sudo -H git clone https://github.com/it-projects-llc/website-addons -b 11.0 others-it-projects-llc-website-addons
+sudo -H git clone https://github.com/akretion/odoo-usability -b 11.0 others-akretion-odoo-usability
+sudo -H git clone https://github.com/jobiols/odoo-addons -b 11.0 jobiols-odoo-addons
+sudo -H git clone https://github.com/regaby/odoo-addons -b 11.0 regaby-odoo-addons
 
 
-#sudo -H pip3 install -r others-akretion-odoo-usability/requirements.txt
 sudo -H pip3 install -r others-it-projects-llc-website-addons/requirements.txt
-#Probar
-sudo -H pip3 install -r jobiols-addons/l10n_ar_export_arba/requirements.txt
-sudo -H pip3 install -r jobiols-addons/l10n_ar_export_sicore/requirements.txt
-sudo -H pip3 install -r regaby-addons/requirements.txt
-#Termina prueba
+#sudo -H pip3 install -r others-akretion-odoo-usability/requirements.txt
+sudo -H pip3 install -r jobiols-odoo-addons/l10n_ar_export_arba/requirements.txt
+sudo -H pip3 install -r jobiols-odoo-addons/l10n_ar_export_sicore/requirements.txt
+sudo -H pip3 install -r regaby-odoo-addons/requirements.txt
 
 sudo chown -R odoo:odoo -R /opt/odoo/addons
 sudo chmod -R 775 /opt/odoo/addons
