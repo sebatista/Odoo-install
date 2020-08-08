3e6be721baa29b344c55dede1a9b1fc82aade659
Sebas <sebatista@yahoo.com>
2020-08-08 19:09:33 -0300

    Update 71_Script_Odoo11_modif_2.0_addons_adhoc.sh
    Se eliminan formas de descargar antiguas.

--- 71_Script_Odoo11_modif_2.0_addons_adhoc.sh
+++ 71_Script_Odoo11_modif_2.0_addons_adhoc.sh
@@ -48,45 +48,6 @@ sudo git clone --depth 1 --branch 11.0 --single-branch https://github.com/ingadh
 sudo git clone --depth 1 --branch 11.0 --single-branch https://github.com/ingadhoc/survey survey
 sudo git clone --depth 1 --branch 11.0 --single-branch https://github.com/ingadhoc/website website
 
-#sudo wget https://github.com/ingadhoc/account-financial-tools/archive/11.0.zip -O ingadhoc-account-financial-tools.zip
-#sudo wget https://github.com/ingadhoc/account-invoicing/archive/11.0.zip -O ingadhoc-account-invoicing.zip
-#sudo wget https://github.com/ingadhoc/account-payment/archive/11.0.zip -O ingadhoc-account-payment.zip
-#sudo wget https://github.com/ingadhoc/aeroo_reports/archive/11.0.zip -O ingadhoc-aeroo_reports.zip
-#sudo wget https://github.com/ingadhoc/argentina-reporting/archive/11.0.zip -O ingadhoc-argentina-reporting.zip
-#sudo wget https://github.com/ingadhoc/argentina-sale/archive/11.0.zip -O ingadhoc-argentina-sale.zip
-#sudo wget https://github.com/ingadhoc/miscellaneous/archive/11.0.zip -O ingadhoc-miscellaneous.zip
-#sudo wget https://github.com/ingadhoc/multi-store/archive/11.0.zip -O ingadhoc-multi-store.zip
-#sudo wget https://github.com/ingadhoc/odoo-argentina/archive/11.0.zip -O ingadhoc-odoo-argentina.zip
-#sudo wget https://github.com/ingadhoc/reporting-engine/archive/11.0.zip -O ingadhoc-reporting-engine.zip
-#sudo wget https://github.com/ingadhoc/sale/archive/11.0.zip -O ingadhoc-sale.zip
-#sudo wget https://github.com/ingadhoc/stock/archive/11.0.zip -O ingadhoc-stock.zip
-
-#sudo unzip ingadhoc-account-financial-tools.zip
-#sudo unzip ingadhoc-account-invoicing.zip
-#sudo unzip ingadhoc-account-payment.zip
-#sudo unzip ingadhoc-aeroo_reports.zip
-#sudo unzip ingadhoc-argentina-reporting.zip
-#sudo unzip ingadhoc-argentina-sale.zip
-#sudo unzip ingadhoc-miscellaneous.zip
-#sudo unzip ingadhoc-multi-store.zip
-#sudo unzip ingadhoc-odoo-argentina.zip
-#sudo unzip ingadhoc-reporting-engine.zip
-#sudo unzip ingadhoc-sale.zip
-#sudo unzip ingadhoc-stock.zip
-
-#sudo rm ingadhoc-account-financial-tools.zip
-#sudo rm ingadhoc-account-invoicing.zip
-#sudo rm ingadhoc-account-payment.zip
-#sudo rm ingadhoc-aeroo_reports.zip
-#sudo rm ingadhoc-argentina-reporting.zip
-#sudo rm ingadhoc-argentina-sale.zip
-#sudo rm ingadhoc-miscellaneous.zip
-#sudo rm ingadhoc-multi-store.zip
-#sudo rm ingadhoc-odoo-argentina.zip
-#sudo rm ingadhoc-reporting-engine.zip
-#sudo rm ingadhoc-sale.zip
-#sudo rm ingadhoc-stock.zip
-
 sudo chown -R odoo:odoo -R /opt/odoo/addons
 sudo chmod -R 775 /opt/odoo/addons
 
